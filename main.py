6ebe4ee47046e33ed1541d7388d69b391dded592
The1Don1 <mathendam@gmail.com>
2024-02-17 14:38:36 +0200

    pulling from api into csv

--- main.py
+++ main.py
@@ -40,22 +40,22 @@ minimum_to_include = 20
 # print(joined_data.head(rows_to_show))
 
 #personalized ratings
-import csv
+# import csv
 
-jabril_rating_dict = {}
-jgb_rating_dict = {}
+# jabril_rating_dict = {}
+# jgb_rating_dict = {}
 
-with open("lab4/jabril-movie-ratings.csv", newline='') as csvfile:
-  ratings_reader = csv.DictReader(csvfile)
-  for row in ratings_reader:
-    if ((row['ratings'] != "") and (float(row['ratings']) > 0) and (float(row['ratings']) < 6)):
-      jabril_rating_dict.update({int(row['item']): float(row['ratings'])})
+# with open("lab4/jabril-movie-ratings.csv", newline='') as csvfile:
+#   ratings_reader = csv.DictReader(csvfile)
+#   for row in ratings_reader:
+#     if ((row['ratings'] != "") and (float(row['ratings']) > 0) and (float(row['ratings']) < 6)):
+#       jabril_rating_dict.update({int(row['item']): float(row['ratings'])})
       
-with open("lab4/jgb-movie-ratings.csv", newline='') as csvfile:
-  ratings_reader = csv.DictReader(csvfile)
-  for row in ratings_reader:
-    if ((row['ratings'] != "") and (float(row['ratings']) > 0) and (float(row['ratings']) < 6)):
-      jgb_rating_dict.update({int(row['item']): float(row['ratings'])})
+# with open("lab4/jgb-movie-ratings.csv", newline='') as csvfile:
+#   ratings_reader = csv.DictReader(csvfile)
+#   for row in ratings_reader:
+#     if ((row['ratings'] != "") and (float(row['ratings']) > 0) and (float(row['ratings']) < 6)):
+#       jgb_rating_dict.update({int(row['item']): float(row['ratings'])})
      
 # print("Rating dictionaries assembled!")
 # print("Sanity check:")
@@ -63,44 +63,76 @@ with open("lab4/jgb-movie-ratings.csv", newline='') as csvfile:
 # print("\tJohn-Green-Bot's rating for 1197 (The Princess Bride) is " + str(jgb_rating_dict[1197]))
 
 #user-user collaborative filtering
-from lenskit.algorithms import Recommender          #import the Recommender class
-from lenskit.algorithms.user_knn import UserUser        #import the UserUser class
+# from lenskit.algorithms import Recommender          #import the Recommender class
+# from lenskit.algorithms.user_knn import UserUser        #import the UserUser class
 
-num_recs =  10        #number of recommendations to make
+# num_recs =  10        #number of recommendations to make
 
 
-user_user = UserUser(15, min_nbrs=3)        #create a UserUser collaborative filtering algorithm with minimum (3) and maximum (15) number of neighbours to consider
+# user_user = UserUser(15, min_nbrs=3)        #create a UserUser collaborative filtering algorithm with minimum (3) and maximum (15) number of neighbours to consider
 
-algo = Recommender.adapt(user_user)         #adapt the UserUser algorithm to the Recommender class
-algo.fit(data.ratings)            #fit the algorithm to the data
+# algo = Recommender.adapt(user_user)         #adapt the UserUser algorithm to the Recommender class
+# algo.fit(data.ratings)            #fit the algorithm to the data
 
-print("Set up a User User collaborative filtering algorithm.")
+# print("Set up a User User collaborative filtering algorithm.")
 
-jab_recs = algo.recommend(-1, num_recs, ratings=pd.Series(jabril_rating_dict))     
-#get recommendations for Jabril, -1 is a placeholder for the user id showing it is not an existing user in the set
-#10 is how many recommendations it should generate
+# jab_recs = algo.recommend(-1, num_recs, ratings=pd.Series(jabril_rating_dict))     
+# #get recommendations for Jabril, -1 is a placeholder for the user id showing it is not an existing user in the set
+# #10 is how many recommendations it should generate
 
-joined_data = jab_recs.join(data.movies['genres'], on='item')
-joined_data = joined_data.join(data.movies['title'], on='item')
-joined_data = joined_data[joined_data.columns[2:]]
-print("\n\nRECOMMENDED FOR JABRIL:")
-print(joined_data)
+# joined_data = jab_recs.join(data.movies['genres'], on='item')
+# joined_data = joined_data.join(data.movies['title'], on='item')
+# joined_data = joined_data[joined_data.columns[2:]]
+# print("\n\nRECOMMENDED FOR JABRIL:")
+# print(joined_data)
 
 #Combine Recommendations lists
-combined_rating_dict = {}
-for k in jabril_rating_dict:
-  if k in jgb_rating_dict:
-    combined_rating_dict.update({k: float((jabril_rating_dict[k]+jgb_rating_dict[k])/2)})
-  else:
-    combined_rating_dict.update({k:jabril_rating_dict[k]})
-for k in jgb_rating_dict:
-   if k not in combined_rating_dict:
-      combined_rating_dict.update({k:jgb_rating_dict[k]})
+# combined_rating_dict = {}
+# for k in jabril_rating_dict:
+#   if k in jgb_rating_dict:
+#     combined_rating_dict.update({k: float((jabril_rating_dict[k]+jgb_rating_dict[k])/2)})
+#   else:
+#     combined_rating_dict.update({k:jabril_rating_dict[k]})
+# for k in jgb_rating_dict:
+#    if k not in combined_rating_dict:
+#       combined_rating_dict.update({k:jgb_rating_dict[k]})
       
-combined_recs = algo.recommend(-1, num_recs, ratings=pd.Series(combined_rating_dict))  #Here, -1 tells it that it's not an existing user in the set, that we're giving new ratings, while 10 is how many recommendations it should generate
+# combined_recs = algo.recommend(-1, num_recs, ratings=pd.Series(combined_rating_dict))  #Here, -1 tells it that it's not an existing user in the set, that we're giving new ratings, while 10 is how many recommendations it should generate
 
-joined_data = combined_recs.join(data.movies['genres'], on='item')      
-joined_data = joined_data.join(data.movies['title'], on='item')
-joined_data = joined_data[joined_data.columns[2:]]
-print("\n\nRECOMMENDED FOR JABRIL / JOHN-GREEN-BOT HYBRID:")
-print(joined_data)
+# joined_data = combined_recs.join(data.movies['genres'], on='item')      
+# joined_data = joined_data.join(data.movies['title'], on='item')
+# joined_data = joined_data[joined_data.columns[2:]]
+# print("\n\nRECOMMENDED FOR JABRIL / JOHN-GREEN-BOT HYBRID:")
+# print(joined_data)
+
+#get API response for movies
+import requests
+
+url = "https://api.themoviedb.org/3/movie/popular?language=en-US"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGY3NGViODBkODA3OWI5ODQ5OTEyN2ExOTU3ZWU4ZSIsInN1YiI6IjY1ZDA3ZTU2ZmNiOGNjMDE2MjQ0YjFiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8_pm3iKxKtm1C-EL0Vc39BUtaq3UwvVcpMc8aeG4osE"
+}
+# Create a list to store all results
+all_data = []
+
+# Loop over the pages you want to get
+for page in range(1, 6):  # This will get pages 1 to 5
+    # Add the page query parameter to the URL
+    response = requests.get(url + "&page=" + str(page), headers=headers)
+    
+    # Convert the JSON response to a Python dictionary
+    data = response.json()
+
+    # Convert the dictionary to a pandas DataFrame
+    df = pd.DataFrame(data['results'])  # 'results' is the key containing the list of movies
+
+    # Append the data from this page to the all_data list
+    all_data.append(df)
+
+# Concatenate all the dataframes in the list into a single dataframe
+all_data_df = pd.concat(all_data, ignore_index=True)
+
+# Save the DataFrame to a CSV file
+all_data_df.to_csv('lab4/movies.csv', index=False)
